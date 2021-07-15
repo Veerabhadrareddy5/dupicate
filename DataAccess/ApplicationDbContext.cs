c024ef2b251cc3827adcb9184276c9c19b500e5e
VEERA BHADRA REDDY KARRI <karriv@usf.edu>
2021-07-15 11:01:45 -0400

    Add files via upload

---
+++ DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DIS_Assignment4_Spring2021.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIS_Assignment4_Spring2021.DataAccess
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Covid_Condition> Covid_Conditions_data { get; set; }
+
+        public DbSet<Covid_Conditions> Covid_Conditions { get; set; }
+        public DbSet<grp_data> grp_data { get; set; }
+    }
+}
