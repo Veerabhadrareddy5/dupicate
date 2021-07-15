7fa43586f3fd9f71e05ae5c1db1fb46d562831c9
VEERA BHADRA REDDY KARRI <karriv@usf.edu>
2021-07-15 11:04:09 -0400

    Add files via upload

---
+++ Models/Covid_Condition.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIS_Assignment4_Spring2021.Models
+{
+
+    public class grp_data
+    {
+        [Key]
+        public int grp_id { get; set; }
+        public string condition_group { get; set; }
+
+        public List<Covid_Condition> Covid_Condition { get; set; }
+
+    }
+
+    public class Covid_Conditions
+    {
+        [Key]
+        public int Id { get; set; }
+        public List<Covid_Condition> data { get; set; }
+    }
+
+    public class Covid_Condition
+    {
+        [Key]
+        public int CaseId { get; set; }
+        public grp_data grp_id { get; set; }
+
+        public string condition_group { get; set; }
+        public string condition { get; set; }
+        //public string icd10_codes { get; set; }
+        public string age_group { get; set; }
+        public string covid_19_deaths { get; set; }
+        //public string number_of_mentions { get; set; }
+        //public string flag { get; set; }
+        public Covid_Conditions Id { get; set; }
+    }
+
+    public class ChartModel
+    {
+        public string ChartType { get; set; }
+        public string Labels { get; set; }
+        public string Data { get; set; }
+        public string Title { get; set; }
+
+    }
+
+}
+
